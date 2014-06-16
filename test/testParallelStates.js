d5802863d877fb2882eb98afb10844039fcf8aa2
Martin Skinner <martin@artcom.de>
2014-06-16 10:09:48 +0200

    expanded parallel state machine tests

--- test/testParallelStates.js
+++ test/testParallelStates.js
@@ -6,7 +6,7 @@ SM.Logger.debug = console.log;
 
 exports['testParallelStates'] = function testParallelStates() {
 
-    // Numlock
+    // Numlock - this is a simple toggle
     var numLockOn = new SM.State("NumLockOn");
     numLockOn.handler.numlock = function(theEvent) {
         return "NumLockOff";
@@ -18,7 +18,7 @@ exports['testParallelStates'] = function testParallelStates() {
     };
     var numLockMachine = new SM.StateMachine(numLockOff, numLockOn);
 
-    // CapsLock
+    // CapsLock - also a simple toggle
     var capsLockOn = new SM.State("CapsLockOn");
     capsLockOn.handler.capslock = function(theEvent) {
         return "CapsLockOff";
@@ -30,7 +30,7 @@ exports['testParallelStates'] = function testParallelStates() {
     };
     var capsLockMachine = new SM.StateMachine(capsLockOff, capsLockOn);
 
-    // Keyboard
+    // Keyboard - can be plugged and unplugged. When plugged, it conatins two toggles: NumLock and CapsLock
     var keyboardOff = new SM.State("KeyboardOff");
     keyboardOff.handler.plug = function(theEvent) {
         return "KeyboardOn";
@@ -42,15 +42,34 @@ exports['testParallelStates'] = function testParallelStates() {
 
     var keyboardMachine = new SM.StateMachine([keyboardOff, keyboardOn]).setup();
 
+    // starts unplugged
     assert.equal("KeyboardOff", keyboardMachine.toString());
     
+    // when plugged, initialize capslock and numlock to off
     keyboardMachine.handleEvent("plug");
     assert.equal("KeyboardOn/(CapsLockOff|NumLockOff)", keyboardMachine.toString());
     
+    // check capslock toggle
     keyboardMachine.handleEvent("capslock");
     assert.equal("KeyboardOn/(CapsLockOn|NumLockOff)", keyboardMachine.toString());
     
     keyboardMachine.handleEvent("capslock");
+    assert.equal("KeyboardOn/(CapsLockOff|NumLockOff)", keyboardMachine.toString());
+
+    // check numlock toggle
+    keyboardMachine.handleEvent("numlock");
+    assert.equal("KeyboardOn/(CapsLockOff|NumLockOn)", keyboardMachine.toString());
+    
+    // now unplug keyboard 
+    keyboardMachine.handleEvent("unplug");
+    assert.equal("KeyboardOff", keyboardMachine.toString());
+    
+    // pressing capslock while unplugged does nothing
+    keyboardMachine.handleEvent("capslock");
+    assert.equal("KeyboardOff", keyboardMachine.toString());
+
+    // plug the keyboard back in and check whether the toggles are back at their initial states 
+    keyboardMachine.handleEvent("plug");
     assert.equal("KeyboardOn/(CapsLockOff|NumLockOff)", keyboardMachine.toString());
 }
 
